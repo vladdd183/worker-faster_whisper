511e4f6eb4c65593b365e3bd4cc059809850ef95
vladdd183 <vladdd183@gmail.com>
2025-04-10 22:25:24 +0300

    Update fetch_models.py

--- builder/fetch_models.py
+++ builder/fetch_models.py
@@ -2,11 +2,11 @@ from concurrent.futures import ThreadPoolExecutor
 from faster_whisper import WhisperModel
 from huggingface_hub import snapshot_download
 
-# repo_id = "efficient-speech/lite-whisper-large-v3-turbo-fast"
-# local_dir = "lite"
-# snapshot_download(repo_id=repo_id, local_dir=local_dir, repo_type="model")
+repo_id = "efficient-speech/lite-whisper-large-v3-turbo-fast"
+local_dir = "lite"
+snapshot_download(repo_id=repo_id, local_dir=local_dir, repo_type="model")
 
-model_names = ["efficient-speech/lite-whisper-large-v3-turbo-fast", "turbo"]
+model_names = ["lite", "turbo"]
 
 
 def load_model(selected_model):
